70c69e8cbe2249fcd52ee3afa94d72357967919a
Verity Anscombe <vanscombe@perse.co.uk>
2021-04-20 11:51:47 +0000

    Stage two requirements completed

--- Game.py
+++ Game.py
@@ -1,15 +1,21 @@
+from itertools import product
+
+class GameError(Exception):
+    
+    pass
+
 
 class Game:
-    
-    _EMPTY = " "
-    P1 = "o"
-    P2 = "x"
+
+    P1 = 'o'
+    P2 = 'x'
+    _EMPTY = ' '
     _DIM = 3
-    
+    DRAW = "d"
+
     def __init__(self):
         self._board = [[Game._EMPTY for _ in range(Game._DIM)] for _ in range(Game._DIM)]
         self._player = Game.P1
-        
 
     def __repr__(self):
         result = "  " + " ".join(str(i+1) for i in range(Game._DIM))
@@ -24,10 +30,11 @@ class Game:
     def play(self,row,col):
         row -= 1
         col -= 1
-        self.__board[row][col] = self.__player
-        self.__player = Game.P1 if self.__player == Game.P2 else Game.P2
-        
-        
+        if self._board[row][col] != Game._EMPTY:
+            raise GameError
+            
+        self._board[row][col] = self._player
+        self._player = Game.P2 if self._player is Game.P1 else Game.P1
     
     @property
     def winner(self):
@@ -42,10 +49,17 @@ class Game:
                 return p
             if all(self._board[i][Game._DIM - 1 - i] is p for i in range(Game._DIM)):
                 return p
-        return None
-     
+        numempty = 0
+        for r,c in product(range(3),range(3)):
+            if self._board[r][c] == Game._EMPTY:
+                numempty += 1
+                
+        if numempty == 0:
+            return Game.DRAW
         
+        return None
 
 if __name__ == "__main__":
     g = Game()
     print(g)
+
